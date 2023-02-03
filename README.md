3ead54555649da12c109b1712760cb21dd20cd29
Steve Jalim <stevejalim@mozilla.com>
2023-02-03 14:59:55 +0000

    Only run the mirroring if the actor is a member of a team associated with the repo
    Requires the REPO secret to be a custom PAT with the scope read:org
    
    This hopefully is a viable way to use this action on a public repo without
    allowing spam Issues to flow to Asana

--- README.md
+++ README.md
@@ -11,7 +11,13 @@ suited to the MozMEAO workflow.
 
 1. Set up your repo's secrets to make `ASANA_PAT` and `ASANA_PROJECT` available - both of these you get from the Asana side. Ideally, the PAT should be an Asana [service account](https://asana.com/guide/help/premium/service-accounts) token.
 
-2. In `your-project/.github/workflows/choose-a-filename.yaml` add:
+2. Ideally using a service account, create a Githib Personal Access Token (eg called `ASANA_GITHUB_BRIDGE_TOKEN`) with these permissions:
+    * `read: org` - to see organization membership and team membership.
+    * `admin: write` - to be able to comment on the original Issue
+
+    If you are using SSO remember to authorize that token for access.
+
+3. In `your-project/.github/workflows/choose-a-filename.yaml` add:
 
 ```code:yaml
 
@@ -26,15 +32,18 @@ jobs:
   handle_issue:
     name: "Trigger following GH Issue change"
     uses: mozmeao/asana-github-bridge/.github/workflows/issue_handler.yaml@APPROPRIATE_VERISON_HERE
+    permissions:
+      repository-projects: "read"
     with:
-      issue-url: ${{ github.event.issue.html_url }}
-      issue-title: ${{ github.event.issue.title }}
+      actor: ${{ github.actor }}
       issue-body: ${{ github.event.issue.body }}
       issue-timestamp: ${{ github.event.issue.updated_at }}
+      issue-title: ${{ github.event.issue.title }}
+      issue-url: ${{ github.event.issue.html_url }}
     secrets:
       ASANA_PAT: ${{ secrets.ASANA_PAT }}
       ASANA_PROJECT: ${{ secrets.ASANA_PROJECT }}
-      REPO_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+      REPO_TOKEN: ${{ secrets.ASANA_GITHUB_BRIDGE_TOKEN }}
 ```
 
 More information will follow as functionality is checked and enabled.
--- bin/manage_asana_task.py
+++ bin/manage_asana_task.py
@@ -18,6 +18,10 @@ ASANA_API_ROOT = "https://app.asana.com/api/1.0/"
 ASANA_TASK_COLLECTION_ENDPOINT = f"{ASANA_API_ROOT}tasks"
 ASANA_PROJECT = environ.get("ASANA_PROJECT")
 
+FLAG_ONLY_REACT_TO_REPO_TEAM = "repo-team"
+FLAG_ONLY_REACT_TO_REPO_ORG = "repo-org"
+FLAG_ONLY_REACT_TO_ALL = "all"
+
 
 # https://developers.asana.com/docs/rich-text#reading-rich-text
 ASANA_ALLOWED_TAGS_FOR_TASKS = {
@@ -42,13 +46,24 @@ def log(message: str) -> None:
     sys.stdout.write("\n")
 
 
-def _get_default_headers() -> Dict:
+def _get_default_asana_headers() -> Dict:
     token = environ.get("ASANA_PAT")
 
     headers = {
         "Accept": "application/json",
         "Content-Type": "application/json",
         "Authorization": f"Bearer {token}",
+    }
+    return headers
+
+
+def _get_default_github_headers() -> Dict:
+    token = environ.get("REPO_TOKEN")
+
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2022-11-28",
     }
     return headers
 
@@ -131,7 +146,6 @@ def create_task(issue_url, issue_title, issue_body, issue_timestamp) -> Tuple[st
         str: the URL of the new task
         bool: whether or not the GH Issue's description was adjusted to suit
             the Asana HTML allowlist
-
     """
 
     task_permalink = "Error creating Asana task"
@@ -153,7 +167,7 @@ def create_task(issue_url, issue_title, issue_body, issue_timestamp) -> Tuple[st
     resp = requests.post(
         ASANA_TASK_COLLECTION_ENDPOINT,
         json=payload,
-        headers=_get_default_headers(),
+        headers=_get_default_asana_headers(),
     )
     if resp.status_code != 201:
         log(resp.content)
@@ -186,19 +200,14 @@ def add_task_as_comment_on_github_issue(
     """Update the original Github issue with a comment linking back
     to the Asana task that it spawned"""
 
-    GITHUB_TOKEN = environ.get("GITHUB_TOKEN")
+    REPO_TOKEN = environ.get("REPO_TOKEN")
 
-    if not GITHUB_TOKEN:
-        log("No GITHUB_TOKEN found - cannot update Issue")
+    if not REPO_TOKEN:
+        log("No REPO_TOKEN found - cannot update Issue")
         return
 
     commenting_url = f"{issue_api_url}/comments"  # NB: no trailing slashj
-
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "Authorization": f"Bearer {GITHUB_TOKEN}",
-        "X-GitHub-Api-Version": "2022-11-28",
-    }
+    headers = _get_default_github_headers()
     comment = f"This issue has been copied to Asana: {task_permalink}"
 
     if github_description_changed_for_asana:
@@ -217,6 +226,71 @@ def add_task_as_comment_on_github_issue(
         log("Asana task URL added in comment on original issue")
 
 
+def _may_bridge_to_asana(actor: str, repo_info: str) -> bool:
+    """If this is used on a public repo, we don't want to sync every issue
+    to Asana. So, for now we'll only sync issues opened by people who are
+    members of teams with access to the repository.
+
+    Args:
+        actor (string):     The username of whoever opened the isue
+        repo_info (string): The name of the org and repo - e.g. octocat/Hello-World
+
+    Returns:
+        bool: Whether the actor is a member of a team associated with the repo
+    """
+
+    only_react_to_flag = environ.get("ONLY_REACT_TO")
+
+    headers = _get_default_github_headers()
+    if only_react_to_flag == FLAG_ONLY_REACT_TO_ALL:
+        # Nothing to check - just forward everyone's issues -- this is probably
+        # only wise on a private repo.
+        log("All Issues are being bridged to Asana.")
+        return
+
+    elif only_react_to_flag == FLAG_ONLY_REACT_TO_REPO_TEAM:
+        # If the actor is in a team that's associated with the repo, we'll allow it
+        repo_teams_url = f"https://api.github.com/repos/{repo_info}/teams"
+
+        raw_teams_resp = requests.get(repo_teams_url, headers=headers)
+        if raw_teams_resp.status_code != 200:
+            log(f"Problem getting teams data for {repo_info}: {raw_teams_resp} {raw_teams_resp.content}")
+            sys.exit(1)
+
+        teams = json.loads(raw_teams_resp.text)
+        _templated_member_urls_for_repo = [x["members_url"] for x in teams if x.get("members_url")]
+        # Trim out the templated member name from the URLs:
+        member_urls_for_repo = [x.replace("{/member}", "") for x in _templated_member_urls_for_repo]
+
+        for members_url in member_urls_for_repo:
+            raw_members_resp = requests.get(members_url, headers=headers)
+            if raw_members_resp.status_code != 200:
+                log(f"Problem getting members data for a team of {repo_info}: {raw_members_resp} {raw_members_resp.content}")
+                sys.exit(1)
+
+            members = [x["login"] for x in json.loads(raw_members_resp.text)]
+            if actor in members:
+                log("Issue actor is on a team associated with the repo.")
+                return True
+
+    elif only_react_to_flag == FLAG_ONLY_REACT_TO_REPO_ORG:
+        # If the actor is in the same org as the repo is, we'll allow it
+        org_name = repo_info.split("/")[0]
+        repo_parent_org_members_url = f"https://api.github.com/orgs/{org_name}/members"
+        raw_org_members_resp = requests.get(repo_parent_org_members_url, headers=headers)
+        if raw_org_members_resp.status_code != 200:
+            log(f"Problem getting member data for {org_name}: {raw_org_members_resp} {raw_org_members_resp.content}")
+            sys.exit(1)
+
+        members = [x["login"] for x in json.loads(raw_org_members_resp.text)]
+        if actor in members:
+            log(f"Issue actor {actor} is part of the {org_name} org that owns this repo.")
+            return True
+
+    log("Bridging this issue to Asana is not allowed")
+    return False
+
+
 def main() -> None:
     """Initially, just create a new Asana Task based on the GH Issue
     related to triggering this action
@@ -225,6 +299,13 @@ def main() -> None:
     * Updating Asana tasks when issues are updated
     * Closing tasks when issues are closed
     """
+
+    github_repo = environ.get("REPO")
+    actor = environ.get("ACTOR")
+
+    if not _may_bridge_to_asana(actor=actor, repo_info=github_repo):
+        log(f"{actor} is not in a team associated with {github_repo}. Not mirroring Issue to Asana")
+        return
 
     issue_url = environ.get("ISSUE_URL")
     issue_title = environ.get("ISSUE_TITLE")
